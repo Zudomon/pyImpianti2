40de58f35f59dbbc7ceb60ef46f5ee113294d0da
Zud <ioZudomon@gmail.com>
2021-11-29 18:40:08 +0100

    commit 4

--- main.py
+++ main.py
@@ -5,12 +5,19 @@
 
 
 def print_hi(name):
+    """
+    :param name: il nome passato come stringa
+    :return: il saluto
+    """
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm 2!')
+    print_hi('Hi PyCharm 2!')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+# printo i grafici UML
+rc = call("./pyreverse_UML.sh", shell=True)
